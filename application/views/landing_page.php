e7a6561d90ff0e006c34a18c042c2c8da1592c5b
firefin0 <47374271+firefin0@users.noreply.github.com>
2019-04-27 07:50:09 +0700

    ini landing page nya

---
+++ application/views/landing_page.php
@@ -0,0 +1,84 @@
+<!-- Masthead -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class="text-uppercase text-white font-weight-bold">Selamat Datang di DOMMA!</h1>
+          <hr class="divider my-4">
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-white-75 font-weight-light mb-5">DOMMA (Dompet Mahasiswa) adalah aplikasi pengatur keuangan yang dikhususkan untuk mahasiswa.</p>
+          <a class="btn btn-dark btn-xl js-scroll-trigger" href="<?php echo base_url('auth/index'); ?>">Masuk</a>&nbsp&nbsp&nbsp
+          <a class="btn btn-light btn-xl js-scroll-trigger" href="<?php echo base_url('auth/registration'); ?>">Daftar</a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  
+
+  <!-- Services Section -->
+  <section class="page-section" id="services">
+  
+    <div class="container">
+      <h2 class="text-center mt-0">Kenapa Menggunakan DOMMA?</h2>
+      <hr class="divider my-4">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-money-check-alt text-dark mb-4"></i>
+            <h3 class="h4 mb-2">Menabung</h3>
+            <p class="text-muted mb-0">Membantu anda untuk menabung.</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-wallet text-dark mb-4"></i>
+            <h3 class="h4 mb-2">Mengatur Uang</h3>
+            <p class="text-muted mb-0">Membantu anda untuk mengatur keuangan.</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-chart-area text-dark mb-4"></i>
+            <h3 class="h4 mb-2">Laporan Keuangan</h3>
+            <p class="text-muted mb-0">Melaporkan keuangan anda secara terperinci.</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-graduation-cap text-dark mb-4"></i>
+            <h3 class="h4 mb-2">Cocok Untuk Mahasiswa</h3>
+            <p class="text-muted mb-0">Cocok untuk anda yang sedang menempuh pendidikan perguruan tinggi.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+ <!-- About Section -->
+ 
+
+  <!-- Contact Section -->
+  <section class="page-section bg-dark" id="contact">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center text-light">
+          <h2 class="mt-0">Contact</h2>
+          <hr class="divider my-4">
+          <p class="text-muted mb-5 text-white-50">Berikut adalah kontak kami yang bisa dihubungi.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 ml-auto text-center">
+          <i class="fas fa-phone fa-3x mb-3 text-muted "></i>
+          <div class="text-white-50">+62 (022) 666-6666</div>
+        </div>
+        <div class="col-lg-4 mr-auto text-center">
+          <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+          <!-- Make sure to change the email address in anchor text AND the link below! -->
+          <a class="d-block" href="mailto:contact@yourwebsite.com">domma@dompetmahasiswa.com</a>
+        </div>
+      </div>
+    </div>
+  </section>
